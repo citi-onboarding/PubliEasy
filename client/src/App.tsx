34b6a62d2a83306cb662625ac6f3099fd1523a9a
Rafaela Leão <rfl5@cin.ufpe.br>
2023-05-25 10:23:17 -0300

    Initial changes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,14 @@
 import React from 'react';
 import { ThemeProvider } from 'styled-components';
 import { theme } from './styles/theme';
-import { GlobalStyle } from './styles/global';
+// import { GlobalStyle } from './styles/global';
 
 
-import { Home } from './pages';
+// import { Home } from './pages';
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <Home/>
-      <GlobalStyle/>
     </ThemeProvider>
   );
 }
