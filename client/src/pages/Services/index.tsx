a4709858fbccf5009d948ab81f0936112a35ad8e
jamillalobo <lobo.jamilla@gmail.com>
2023-05-26 21:44:31 -0300

    fixed tags from styled components of index

--- client/src/pages/Services/index.tsx
+++ client/src/pages/Services/index.tsx
@@ -1,21 +1,37 @@
 import React from "react";
-import { ServicesContainer, ServicesH1, ServicesP, ServicesImg, HorizontalLine } from './styles';
+import { ServicesContainer, ServicesH1, ServicesP, ServicesImg, HorizontalLine, ItemImg, Title, Images } from './styles';
 import { Product, Product2 } from '../../assets';
 
 
 export const Service: React.FC = () => {
     return (
         <ServicesContainer>
-            <ServicesH1>Nosso Produto</ServicesH1>
-            <HorizontalLine></HorizontalLine>
-            <ServicesImg src={Product} alt="" />
-            <ServicesImg src={Product2} alt="" />
-            <ServicesImg src={Product} alt="" />
-            <ServicesImg src={Product2} alt="" />
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
-            <ServicesP>
-                <strong>Lorem ipsum</strong>
-            </ServicesP>
+            <Title>
+                <ServicesH1>Nosso Serviço</ServicesH1>
+                <HorizontalLine></HorizontalLine>    
+            </Title>
+            <Images>
+                <ServicesImg>
+                    <ItemImg src={Product} alt="" />
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
+                    <ServicesP><strong>Lorem ipsum</strong></ServicesP>
+                </ServicesImg>
+                <ServicesImg>
+                    <ItemImg src={Product2} alt="" />
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
+                    <ServicesP><strong>Lorem ipsum</strong></ServicesP>
+                </ServicesImg>
+                <ServicesImg>
+                    <ItemImg src={Product} alt="" />
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
+                    <ServicesP><strong>Lorem ipsum</strong></ServicesP>
+                </ServicesImg>
+                <ServicesImg>
+                    <ItemImg src={Product2} alt="" />
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
+                    <ServicesP><strong>Lorem ipsum</strong></ServicesP>
+                </ServicesImg>
+            </Images>
         </ServicesContainer>
     );
-    }
+}
--- client/src/pages/Services/styles.ts
+++ client/src/pages/Services/styles.ts
@@ -2,31 +2,60 @@ import styled from 'styled-components';
 
 
 export const ServicesContainer = styled.div`
-    width: 100vw;
-    height: 100vh;
+    width: 1440px;
+    height: auto;
 
-    display: block;
-    flex-direction: row;
+    display: flex;
+    flex-direction: column;    
 
-    
 `;
 
+export const Title = styled.div`
+
+`;
+
+export const Images = styled.div`
+    display: flex;
+    flex-direction: row;
+    position: absolute;
+    top: 200px;
+    left: 215xp;
+
+`;
+export const ServicesImg = styled.div`
+    width: 25%;
+
+    display: flex;
+    flex-direction: column;
+`;
+export const ItemImg = styled.img`
+
+`;
+
+
 export const HorizontalLine = styled.div`
-    width: 40%;
-    height: 3px;
-    background: #000;
-`
+    width: 450px;
+    height: 2px;
+    background: linear-gradient(to right, #7E3BC7, #D436CB);
+    position: absolute;
+    left: 192px;
+    top: 130px;
+    `;
     
 export const ServicesH1 = styled.h1`  
-    font-size: 54px;
+    font-size: 46px;
     color: ${(props) => props.theme.colors.primary};
     margin-top: 40px;
     text-align: left;
-
-    @media (max-width: 720px) {
-        font-size: 28px;
-    }
-
+    font-family: Raleway;
+    font-weight: 500;
+    line-height: 54px;
+    letter-spacing: 0em;
+    position: absolute;
+    width: 307px;
+    height: 54px;
+    left: 192px;
+    top: 37px;
 `;
 
 export const ServicesP = styled.p`
@@ -34,17 +63,6 @@ export const ServicesP = styled.p`
     margin-top: 24px;
     line-height: 32px;
     text-align: left;
-
-    @media (max-width: 720px) {
-      font-size: 16px;
-      margin-top: 0;
-    }
+    left: 215px;
 `;
 
-export const ServicesImg = styled.img`
-    img {
-        width: 25%;
-        display: inline-block;
-        justify-content: space-around;
-    }
-`;
