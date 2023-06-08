1a5d78f5e833ee70953b3431b45f26662f5cfe21
jamillalobo <lobo.jamilla@gmail.com>
2023-06-08 16:37:25 -0300

    FEAT[feature/metricsBck]: added new columns for metrics

--- server/src/models/Metric.ts
+++ server/src/models/Metric.ts
@@ -12,83 +12,16 @@ export class Metric {
     @Column()
     metricDescription: string
 
+    @Column()
+    metric2: string
+
+    @Column()
+    metricDescription2: string
+
+    @Column()
+    metric3: string
+
+    @Column()
+    metricDescription3: string
+
 }
-
-// @Entity()
-// export class Metric {
-
-//     @PrimaryGeneratedColumn()
-//     id: number
-
-//     @Column()
-//     metric: string
-
-//     @Column()
-//     metricDescription: string
-
-// }
-// @Entity()
-// export class Metric {
-
-//     @PrimaryGeneratedColumn()
-//     id: number
-
-//     @Column()
-//     metric: string
-
-//     @Column()
-//     metricDescription: string
-
-// }
-// @Entity()
-// export class Metric {
-
-//     @PrimaryGeneratedColumn()
-//     id: number
-
-//     @Column()
-//     metric: string
-
-//     @Column()
-//     metricDescription: string
-
-// }
-// @Entity()
-// export class Metric {
-
-//     @PrimaryGeneratedColumn()
-//     id: number
-
-//     @Column()
-//     metric: string
-
-//     @Column()
-//     metricDescription: string
-
-// }
-// @Entity()
-// export class Metric {
-
-//     @PrimaryGeneratedColumn()
-//     id: number
-
-//     @Column()
-//     metric: string
-
-//     @Column()
-//     metricDescription: string
-
-// }
-// @Entity()
-// export class Metric {
-
-//     @PrimaryGeneratedColumn()
-//     id: number
-
-//     @Column()
-//     metric: string
-
-//     @Column()
-//     metricDescription: string
-
-// }
