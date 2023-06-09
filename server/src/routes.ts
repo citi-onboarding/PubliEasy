ab3afb2835268215c691e915e71010a830789001
Rafaela Leão <rfl5@cin.ufpe.br>
2023-06-09 17:04:56 -0300

    feature/nodemailerBackCorrect with out Users

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,14 +1,7 @@
 import express from 'express';
-import UserController from '@controllers/UserController'
 import { SendMail } from '@controllers/MailController';
 
 const routes = express.Router();
-const userController = new UserController();
-
-routes.post('/user', userController.create);
-routes.get('/user', userController.get);
-routes.delete('/user/:id', userController.delete);
-routes.put('/user/:id', userController.update);
 
 routes.post('/email', SendMail);
 
