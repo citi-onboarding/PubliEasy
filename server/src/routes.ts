369b164d783d4e2c9a282f17991ff19dfcf3dc14
Renato Serrano <renatom.sandrade@gmail.com>
2023-06-12 16:33:30 -0300

    fixed stuff

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,12 +1,21 @@
 import express from 'express';
 import ServicesController from '@controllers/ServicesController';
+import MetricController from '@controllers/MetricController';
+
 
 const routes = express.Router();
 const servicesController = new ServicesController();
+const metricController = new MetricController();
+
 
 routes.post('/service',servicesController.create);
 routes.get('/service',servicesController.get);
 routes.delete('/service/:id',servicesController.delete);
 routes.put('/service/:id',servicesController.update);
+
+routes.post('/metric',metricController.create);
+routes.get('/metric',metricController.get);
+routes.delete('/metric/:id',metricController.delete);
+routes.put('/metric/:id',metricController.update);
 
 export default routes;
